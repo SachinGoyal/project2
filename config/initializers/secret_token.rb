ae5f14863fad06e62d05012d67f6f6bd5a5b06b5
sachin <jony.malhotra82@gmail.com>
2014-01-03 15:15:45 +0530

    avc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BranchApp::Application.config.secret_token = 'f42585ef3059c8ef4d298cefe216530c3c978e6cf621048f136d77bc5ca3b6ccaf9c9e034dc8ec616cec938d270e260a8ec20c989ee883c0bdf641ebf1abc114'
