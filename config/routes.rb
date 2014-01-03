32b5a1a3ab7da6e8af1c1af4814f8a5b9a8411f1
sachin <jony.malhotra82@gmail.com>
2014-01-03 13:42:16 +0530

    wesfd

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Project2::Application.routes.draw do
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  #evserdfgvnlekdfnvahjdv
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
